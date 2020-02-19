fd9d21342d6f1dfc44b33d72f5e8966710c8017f
prabhu_nath <prabhu_nath.tiwary@aponiar.com>
2020-02-19 19:03:11 +0530

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-/*
-import 'package:flutter/cupertino.dart';
+/*import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -37,10 +36,12 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   @override
   Widget build(BuildContext context) {
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
+
       floatingActionButton: new FloatingActionButton(
         onPressed: () {
           _settingModalBottomSheet(context);
@@ -53,131 +54,50 @@ class _MyHomePageState extends State<MyHomePage> {
 
 void _settingModalBottomSheet(context) {
 
+
   showModalBottomSheet<dynamic>(
     isScrollControlled: true,
+    backgroundColor: Colors.transparent,
+    isDismissible: true,
+      useRootNavigator: true,
       context: context,
       builder: (BuildContext context) {
-        return SafeArea(
-          child: Container(
-            color: Color(0xFF737373),
-            height: (MediaQuery.of(context).size.height)*0.5,
-            width: (MediaQuery.of(context).size.width) *0.5,
-            child: Container(
-              margin: EdgeInsets.only(left: 20.0,right: 20.0),
-
-              decoration: BoxDecoration(
-                color: Theme.of(context).canvasColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: const Radius.circular(10.0),
-                  topRight: const Radius.circular(10.0),
-                )
-              ),
-
-              child: new Wrap(
-                children: <Widget>[
-                  new ListTile(
-                      leading: new Icon(Icons.music_note),
-                      title: new Text('Music'),
-                      onTap: () {
-                        return
-                        print('clicked on music');
-                      }
-                      ),
-                  new ListTile(
-                      leading: new Icon(Icons.music_note),
-                      title: new Text('Music'),
-                      onTap: () {
-                        return
-                          print('clicked on music');
-                      }
-                  ),
-                  new ListTile(
-                      leading: new Icon(Icons.music_note),
-                      title: new Text('Music'),
-                      onTap: () {
-                        return
-                          print('clicked on music');
-                      }
-                  ),
-                  new ListTile(
-                      leading: new Icon(Icons.music_note),
-                      title: new Text('Music'),
-                      onTap: () {
-                        return
-                          print('clicked on music');
-                      }
-                  ),
-                  new ListTile(
-                      leading: new Icon(Icons.music_note),
-                      title: new Text('Music'),
-                      onTap: () {
-                        return
-                          print('clicked on music');
-                      }
-                  ),
-                  new ListTile(
-                      leading: new Icon(Icons.music_note),
-                      title: new Text('Music'),
-                      onTap: () {
-                        return
-                          print('clicked on music');
-                      }
-                  ),
-                  new ListTile(
-                      leading: new Icon(Icons.music_note),
-                      title: new Text('Music'),
-                      onTap: () {
-                        return
-                          print('clicked on music');
-                      }
-                  ),
-                  new ListTile(
-                      leading: new Icon(Icons.music_note),
-                      title: new Text('Music'),
-                      onTap: () {
-                        return
-                          print('clicked on music');
-                      }
-                  ),
-                  new ListTile(
-                      leading: new Icon(Icons.music_note),
-                      title: new Text('Music'),
-                      onTap: () {
-                        return
-                          print('clicked on music');
-                      }
-                  ),
-                  new ListTile(
-                      leading: new Icon(Icons.music_note),
-                      title: new Text('Music'),
-                      onTap: () {
-                        return
-                          print('clicked on music');
-                      }
-                  ),
-                  new ListTile(
-                    leading: new Icon(Icons.videocam),
-                    title: new Text('Video'),
-                    onTap: () {
-                      return
-                        print('clicked on video');
-
+        return
+          Container(
+            constraints:  BoxConstraints(minHeight: (MediaQuery.of(context).size.height)* 0.2, maxHeight: (MediaQuery.of(context).size.height)* 0.3),
+            child: DraggableScrollableSheet(
+              expand: true,
+              initialChildSize: 1.0,
+              minChildSize: 0.0,
+              maxChildSize: 1.0,
+              builder: (BuildContext context, myscrollController) {
+                return Container(
+                  color: Colors.tealAccent[200],
+                  child: ListView.builder(
+                    controller: myscrollController,
+                    itemCount: 25,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                          title: Text(
+                            'Dish $index',
+                            style: TextStyle(color: Colors.black54),
+                          ),
+                      );
                     },
                   ),
-                ],
-              ),
+                );
+              },
             ),
-          ),
         );
 
-      });
-}
-*/
+      }
+
+      );
+}*/
 
 import 'package:flutter/material.dart';
-void main() => runApp(
-    DragabbleScrollableSheetDemo()
-);
+
+void main() => runApp(DragabbleScrollableSheetDemo());
 
 class DragabbleScrollableSheetDemo extends StatefulWidget {
   @override
@@ -187,8 +107,9 @@ class DragabbleScrollableSheetDemo extends StatefulWidget {
 
 class _DragabbleScrollableSheetDemoState
     extends State<DragabbleScrollableSheetDemo> {
+  bool isGestureClicked = false;
+  bool isFloatingButtonClicked = false;
 
-  bool isClicked = false;
   @override
   void initState() {
     // TODO: implement initState
@@ -200,84 +121,63 @@ class _DragabbleScrollableSheetDemoState
     return MaterialApp(
       darkTheme: ThemeData.dark(),
       theme: ThemeData(brightness: Brightness.dark),
-      home: Scaffold(
-
-        appBar: AppBar(
-          title: const Text('DraggableScrollableSheet'),
-        ),
-
+      home: GestureDetector(
+        onTap: () {
+          setState(() {
+            isGestureClicked = true;
+            isFloatingButtonClicked = false;
+            _settingModalBottomSheet(context);
+          });
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('DraggableScrollableSheet'),
+          ),
           floatingActionButton: new FloatingActionButton(
             onPressed: () {
               setState(() {
+                isGestureClicked = false;
+                isFloatingButtonClicked = true;
                 _settingModalBottomSheet(context);
-
               });
             },
             child: new Icon(Icons.add),
           ),
-
-        body: isClicked?_settingModalBottomSheet(context):Container(),
-        /*Container(
-          child: DraggableScrollableSheet(
-            initialChildSize: 0.3,
-            minChildSize: 0.1,
-            maxChildSize: 1.0,
-            builder: (BuildContext context, myscrollController) {
-              return Container(
-                color: Colors.tealAccent[200],
-                child: ListView.builder(
-                  controller: myscrollController,
-                  itemCount: 25,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                        title: Text(
-                          'Dish $index',
-                          style: TextStyle(color: Colors.black54),
-                        ));
-                  },
-                ),
-              );
-            },
-          ),
-        ),*//*
-        Positioned(
-
-          child: FloatingActionButton(onPressed: (){
-
-          }
-          ),
-        )*/
+          body: _settingModalBottomSheet(context),
+        ),
       ),
     );
   }
 
-   _settingModalBottomSheet(BuildContext context) {
-    setState(() {
-      isClicked =true;
-    });
-
-    return Container(
-          child: DraggableScrollableSheet(
-            initialChildSize: 0.3,
-            minChildSize: 0.0,
-            maxChildSize: 0.8,
-            builder: (BuildContext context, myscrollController) {
-              return Container(
-                color: Colors.tealAccent[200],
-                child: ListView.builder(
-                  controller: myscrollController,
-                  itemCount: 25,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                        title: Text(
-                          'Dish $index',
-                          style: TextStyle(color: Colors.black54),
-                        ));
-                  },
-                ),
-              );
-            },
-          ),
-        );
+  _settingModalBottomSheet(BuildContext context) {
+    if(isGestureClicked && !isFloatingButtonClicked){
+      return Container();
+    }
+    else if (isFloatingButtonClicked && !isGestureClicked){
+      return Container(
+        child: DraggableScrollableSheet(
+          initialChildSize: 0.3,
+          minChildSize: 0.0,
+          maxChildSize: 0.8,
+          builder: (BuildContext context, myscrollController) {
+            return Container(
+              color: Colors.tealAccent[200],
+              child: ListView.builder(
+                controller: myscrollController,
+                itemCount: 25,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                      title: Text(
+                        'Dish $index',
+                        style: TextStyle(color: Colors.black54),
+                      ));
+                },
+              ),
+            );
+          },
+        ),
+      );
+    }else
+   Container();
   }
 }
